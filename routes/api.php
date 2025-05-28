6bbd7e43c822cd368716491baa903a7998732b18
Ulvis <nutzys33@gmail.com>
2025-05-28 13:55:17 +0300

    Sanctum Auth added

--- routes/api.php
+++ routes/api.php
@@ -7,5 +7,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [RegisteredUserController::class, 'store']);
 Route::post('/login', [AuthenticatedSessionController::class, 'store']);
-Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
-Route::get('/user', [UserController::class, 'show']);
+Route::get('/user', [UserController::class, 'show'])->middleware('auth:sanctum');
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->middleware('auth:sanctum');
+
