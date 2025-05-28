f5b16d2c7f409a8ce8e0679a603b1ba3761b1863
Ulvis <nutzys33@gmail.com>
2025-05-28 13:53:58 +0300

    statefulApi for sanctum added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
